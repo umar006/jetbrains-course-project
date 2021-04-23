3e320409addaa0583d2f47235eaaa2e43571d92c
umar006 <umaralfaruq597@gmail.com>
2021-04-23 11:35:14 +0700

    completed first stage

--- kotlin-developer/cinema-room-manager/Cinema Room Manager/task/src/cinema/Cinema.kt
+++ kotlin-developer/cinema-room-manager/Cinema Room Manager/task/src/cinema/Cinema.kt
@@ -1,5 +1,15 @@
 package cinema
 
 fun main() {
-    // write your code here
+    println("""
+        Cinema:
+          1 2 3 4 5 6 7 8
+        1 S S S S S S S S
+        2 S S S S S S S S
+        3 S S S S S S S S
+        4 S S S S S S S S
+        5 S S S S S S S S
+        6 S S S S S S S S
+        7 S S S S S S S S
+    """.trimIndent())
 }
