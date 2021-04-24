44206def980a5f259a846c45e8abfbca59707011
umar006 <umaralfaruq597@gmail.com>
2021-04-24 11:00:54 +0700

    completed first stage

--- kotlin-developer/coffee-machine/Coffee Machine/task/src/machine/Main.kt
+++ kotlin-developer/coffee-machine/Coffee Machine/task/src/machine/Main.kt
@@ -1,5 +1,11 @@
 package machine
 
 fun main() {
-    println("Hello, world!")
+    println("Starting to make a coffee")
+    println("Grinding coffee beans")
+    println("Boiling water")
+    println("Mixing boiled water with crushed coffee beans")
+    println("Pouring coffee into the cup")
+    println("Pouring some milk into the cup")
+    println("Coffee is ready!")
 }
