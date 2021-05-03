8234765069f0742a47784b8b437f807fd2e3656e
umar006 <umaralfaruq597@gmail.com>
2021-05-03 15:39:03 +0700

    completed fourth stage

--- kotlin-developer/smart-calculator/Smart Calculator/task/src/calculator/Main.kt
+++ kotlin-developer/smart-calculator/Smart Calculator/task/src/calculator/Main.kt
@@ -7,14 +7,36 @@ fun main() {
         if (value == "/exit") break
         if (value == "") continue
         if (value == "/help") {
-            println("The program calculates the sum of numbers")
+            println("The program calculates the sum and the subtraction depends on the input")
             continue
         }
 
-        val inputList: List<Int> = value.split(" ").map { it.toInt() }
-        var sum = 0
-        for (input in inputList) sum += input
-        println(sum)
+        val inputList = value.split("\\s+".toRegex()).toMutableList()
+        val operators: ArrayList<String> = ArrayList()
+        val numbers: ArrayList<Int> = ArrayList()
+
+        for (input in inputList) {
+            if (input.last().isDigit()) numbers.add(input.toInt())
+            else operators.add(input)
+        }
+
+        for (idx in operators.indices) {
+            while (operators[idx].length > 1) {
+                if ("++" in operators[idx]) operators[idx] = operators[idx].replace("++", "+")
+                if ("--" in operators[idx]) operators[idx] = operators[idx].replace("--", "+")
+                if ("+-" in operators[idx]) operators[idx] = operators[idx].replace("+-", "-")
+                if ("-+" in operators[idx]) operators[idx] = operators[idx].replace("-+", "-")
+            }
+        }
+
+        var result = numbers[0]
+        for (ix in operators.indices) {
+            when (operators[ix]) {
+                "+" -> result += numbers[ix + 1]
+                "-" -> result -= numbers[ix + 1]
+            }
+        }
+        println(result)
     }
     println("Bye!")
 }
